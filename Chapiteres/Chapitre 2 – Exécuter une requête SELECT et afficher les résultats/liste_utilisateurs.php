7d2bc694912263d7294296a796c3479a0fe31594
Yahia <yahiaelafia1@gmail.com>
2026-03-24 10:51:26 +0100

    PHP

---
+++ Chapiteres/Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/liste_utilisateurs.php
@@ -0,0 +1,52 @@
+<?php 
+
+require 'connexion.php';
+
+
+try {
+    $sql = "SELECT * FROM users";
+
+    $stmt = $pdo->query($sql);
+
+    $utilisateurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    // foreach ($utilisateurs as $user) {
+    //     echo "ID : " . $user['id'] . " -Nom : " . $user['nom'] . " -Email : " . $user['email'] . "<br>";
+    // }
+
+
+echo "<table border='1'>
+
+<tr>
+<th>ID_USER</th>
+<th>Nom</th>
+<th>Email</th>
+<th>Date_birth</th>
+</tr>";
+
+foreach ($utilisateurs as $user) {
+    echo "<tr>
+    <td>{$user['id_user']}</td>
+    <td>{$user['user_name']}</td>
+    <td>{$user['email']}</td>
+    <td>{$user['date_birth']}</td>
+    </tr>";
+}
+
+echo "</table>";
+
+
+
+
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+
+
+
+
+
+
+?>
